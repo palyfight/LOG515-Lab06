ed404241d6dbfa9573b734a9972bcdd4f7b24cdb
Chris Bama <chrisbama92@hotmail.ca>
2016-11-05 12:35:00 -0400

    First commit

---
+++ lab06/src/main/java/LOG515/lab06/App.java
@@ -0,0 +1,13 @@
+package LOG515.lab06;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
