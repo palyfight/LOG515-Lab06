a621fec4350f3d27bef84e2a2bdc91e78aebbb40
Sebastien Morais <smorais93@hotmail.com>
2016-11-16 22:29:33 -0500

    try #2

---
+++ lab06/src/main/java/LOG515/lab06/Chat.java
@@ -0,0 +1,39 @@
+package LOG515.lab06;
+
+import org.eclipse.jetty.websocket.api.*;
+import org.json.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+
+import static j2html.TagCreator.*;
+import static spark.Spark.*;
+
+public class Chat {
+	// this map is shared between sessions and threads, so it needs to be thread-safe (http://stackoverflow.com/a/2688817)
+    static Map<Session, String> userUsernameMap = new ConcurrentHashMap();
+    static int nextUserNumber = 1; //Used for creating the next username
+
+  //Sends a message from one user to all users, along with a list of current usernames
+    public static void broadcastMessage(String sender, String message) {
+        userUsernameMap.keySet().stream().filter(Session::isOpen).forEach(session -> {
+            try {
+                session.getRemote().sendString(String.valueOf(new JSONObject()
+                    .put("userMessage", createHtmlMessageFromSender(sender, message))
+                    .put("userlist", userUsernameMap.values())
+                ));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+    }
+
+    //Builds a HTML element with a sender-name, a message, and a timestamp,
+    private static String createHtmlMessageFromSender(String sender, String message) {
+        return article().with(
+                b(sender + " says:"),
+                p(message),
+                span().withClass("timestamp").withText(new SimpleDateFormat("HH:mm:ss").format(new Date()))
+        ).render();
+    }
+}
